8a069fb2e6e78ee51a27048889434334bead36aa
Jose <joseeenrique.jm@gmail.com>
2022-11-08 20:43:45 -0700

    finished [roduct.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -22,6 +22,22 @@ product_name:{
   type:DataTypes.STRING,
   allowNull:false
 },
+
+price:{
+  type: DataTypes.DECIMAL,
+  allowNull: false
+},
+
+stock: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  defaultValue: 10
+},
+
+category_id: {
+  type: DataTypes.INTEGER,
+  allowNull: false
+},
   {
     sequelize,
     timestamps: false,
